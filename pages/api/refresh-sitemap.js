193d9662e6fa9ea590f3ca52c57c6d69c3bbd2f4
DrojasFrontend <daniel.rojas@leggercolombia.com>
2024-09-18 12:37:07 -0500

    remove files 2

--- pages/api/refresh-sitemap.js
+++
@@ -1,9 +0,0 @@
-// /pages/api/refresh-sitemap.js
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    // Aquí puedes invalidar el caché o disparar la actualización del sitemap
-    res.status(200).json({ message: 'Sitemap refreshed' });
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
-  }
-}
