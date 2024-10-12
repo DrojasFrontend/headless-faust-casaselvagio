becc421220253e272d22380028ea3ea05e17faa3
DrojasFrontend <daniel.rojas@leggercolombia.com>
2024-10-12 08:56:04 -0500

    New menu PROD

--- components/UI/Header/HeaderWhite/HeaderWhite.js
+++ components/UI/Header/HeaderWhite/HeaderWhite.js
@@ -76,7 +76,7 @@ export default function Header({
 					</div>
 
 					<div className={cx("right")}>
-						{menuHeaderItems?.slice(3, 7).map((m, idx) => {
+						{menuHeaderItems?.slice(4, 7).map((m, idx) => {
 							return (
 								<Link href={m?.path} key={idx}>
 									<a className={cx("link", m?.cssClasses )}>{m?.label}</a>
